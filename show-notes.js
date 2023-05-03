7e855f2f62825d447629f4bc001be4c91b281a66
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-05-02 22:58:04 -0400

    almost done, backend logic confirmed

--- show-notes.js
+++ show-notes.js
@@ -1,51 +1,49 @@
-
 var searchQuery = "";
 const notes = JSON.parse(localStorage.getItem('notes') || '[]');
-const indexexToShow = [];
-console.log(notes[0].title);
-function search() {
-    // Retrieve the value of the search input field
-    searchQuery = document.getElementById("note-title").value;
-    
-    // Do something with the search query, such as display search results
-    
-    // Clear the search input field
-    document.getElementById("note-title").value = "";
-    showNotes(searchQuery);
+const indexToShow = [];
+// const keys = Object.keys(notes[0]);
+// console.log(keys);
+
+function get_index(title) {
+  for (let i=0; i<notes.length; i++) {
+    if (title == notes[i].title) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-function showNotes() {
-    document.querySelectorAll('.note').forEach(note => note.remove());
-    notes.forEach((note, index)=>{
-        let liEl=`<li class="note">
-                        <div class="details">
-                            <p>${note.title}</p>
-                            <span>${note.description}</span>
-                        </div>
-                        <div class="bottom-content">
-                            <span>${note.date}</span>
-                            <div class="settings">
-                                <i onClick="updateNote(${index}, '${note.title}', '${note.description}')"  class="uil uil-edit"></i>
-                                <i onClick="deleteNote(${index})" class="uil uil-trash"></i>
-                            </div>
-                        </div>
-                    </li>`;
-        addBox.insertAdjacentHTML('afterend', liEl);
-    });
+function search() {
+  // Retrieve the value of the search input field
+  searchQuery = document.getElementById("note-title").value;
+
+  // Do something with the search query, such as display search results
+
+  // Clear the search input field
+  document.getElementById("note-title").value = "";
+  showNotes(searchQuery);
+  noteShow();
+}
+
+function noteShow() {
+  for (let i = 0; i < indexToShow.length; i++) {
+    let noteTitle = notes[indexToShow[i]].description;
+    console.log(noteTitle);
+  }
 }
 
 function showNotes(notetitle) {
-    if (notetitle == "") {
-        window.alert("No title specified!");
-    }
-    i = 0;
+  indexToShow.length = 0; // Clear the array before populating it again
+  if (notetitle == "") {
+    window.alert("No title specified!");
+  }
+  else {
+    let i = 0; // Declare a local variable for i
     while (i < notes.length) {    
-        if (notetitle[i].title == notetitle) {
-            indexexToShow.push(i);
-            i = i + 1;
-        }
-        else {
-            i = i + 1;
-        }
+      if (notes[i].title == notetitle) {
+        indexToShow.push(i);
+      }
+      i++;
     }
-} 
+  }
+}
